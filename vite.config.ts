a3430471f6e2ce7276819c94c319fc4c59a86934
Dhyan <dhyanjar@users.noreply.github.com>
2026-08-09 23:14:33 +0000

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Dhyan_Portfolio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
