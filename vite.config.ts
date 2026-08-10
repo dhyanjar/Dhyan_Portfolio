76df2d1a156ddbe027df9870d5d3c22cf67517cc
Dhyan <dhyanjar@users.noreply.github.com>
2026-08-10 14:26:33 +0000

    Revert "resolve"
    This reverts commit 02ee08312bee0626f1ad04501dddb423380e6157.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Dhyan_Portfolio/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
